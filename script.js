c9a3cc73c1e8bd5c70d6b74ec78deafad173ff18
Anthony Stender <anthony.m.stender@gmail.com>
2024-03-02 20:51:21 -0600

    Commit

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Set the date we're counting down to
   const countDownDate = new Date("April 17, 2024 22:00:00").getTime();
 
   // Update the countdown every 1 second
-  const x = setInterval(function() {
-      // Get the current date and time
-      const now = new Date().getTime();
+  const x = setInterval(function () {
+    // Get the current date and time
+    const now = new Date().getTime();
 
-      // Calculate the time remaining
-      const distance = countDownDate - now;
+    // Calculate the time remaining
+    const distance = countDownDate - now;
 
-      // Calculate days, hours, minutes, and seconds
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Calculate days, hours, minutes, and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Display the countdown
-      document.getElementById("countdown").innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
+    // Display the countdown
+    document.getElementById(
+      "countdown"
+    ).innerHTML = `${days}d: ${hours}h: ${minutes}m: ${seconds}s`;
 
-      // If the countdown is over, display a message
-      if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("countdown").innerHTML = "EXPIRED";
-      }
+    // If the countdown is over, display a message
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "IT'S OVER";
+    }
   }, 1000);
 });
